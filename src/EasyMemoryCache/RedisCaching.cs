be5f06355fe8ed1821965e3955203e30b2d7653b
Konrad Gąska <konrad.gaska@avenga.com>
2022-03-21 17:16:43 +0100

    Fixed SemaphoreSlim usage and GetValueFromCache Redis implementation

--- src/EasyMemoryCache/RedisCaching.cs
+++ src/EasyMemoryCache/RedisCaching.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.StackExchangeRedis;
@@ -11,11 +10,11 @@ using StackExchange.Redis;
 
 namespace EasyMemoryCache
 {
-    public class RedisCaching : ICaching
+    public class RedisCaching : ICaching, IDisposable
     {
         private readonly IDistributedCache _myCache;
         private readonly IServer _server;
-        private readonly SemaphoreSlim _cacheLock = new SemaphoreSlim(1);
+        private readonly NamedSemaphoreSlim _cacheLock = new NamedSemaphoreSlim(1);
 
         public RedisCaching(IDistributedCache cache, IServer server)
         {
@@ -35,24 +34,37 @@ namespace EasyMemoryCache
 
             if (data == null || EqualityComparer<T>.Default.Equals(cachedObject, default))
             {
-                cachedObject = objectSettingFunction();
-                var serializedObject = JsonConvert.SerializeObject(cachedObject);
-                var entryOptions = new DistributedCacheEntryOptions()
+                _cacheLock[cacheItemName].Wait();
+                try
                 {
-                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(cacheTimeInMinutes)
-                };
+                    cachedObject = objectSettingFunction();
+                    var serializedObject = JsonConvert.SerializeObject(cachedObject);
+                    var entryOptions = new DistributedCacheEntryOptions()
+                    {
+                        AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(cacheTimeInMinutes)
+                    };
 
-                var oType = cachedObject.GetType();
-                if (oType.IsGenericType && (oType.GetGenericTypeDefinition() == typeof(List<>)))
-                {
-                    if (((ICollection)cachedObject).Count > 0 || cacheEmptyList)
+                    var oType = cachedObject.GetType();
+                    if (oType.IsGenericType && (oType.GetGenericTypeDefinition() == typeof(List<>)))
+                    {
+                        if (((ICollection)cachedObject).Count > 0 || cacheEmptyList)
+                        {
+                            _myCache.SetString(cacheItemName, serializedObject, entryOptions);
+                        }
+                    }
+                    else
                     {
                         _myCache.SetString(cacheItemName, serializedObject, entryOptions);
                     }
                 }
-                else
+                catch (Exception err)
                 {
-                    _myCache.SetString(cacheItemName, serializedObject, entryOptions);
+                    Console.WriteLine(err.Message);
+                    return cachedObject;
+                }
+                finally
+                {
+                    _cacheLock[cacheItemName].Release();
                 }
             }
             return cachedObject;
@@ -62,9 +74,7 @@ namespace EasyMemoryCache
         {
             T cachedObject = default;
 
-            await _cacheLock.WaitAsync().ConfigureAwait(false);
             var data = await _myCache.GetStringAsync(cacheItemName);
-
             if (data != null)
             {
                 cachedObject = JsonConvert.DeserializeObject<T>(data);
@@ -72,6 +82,7 @@ namespace EasyMemoryCache
 
             if (data == null || EqualityComparer<T>.Default.Equals(cachedObject, default))
             {
+                await _cacheLock[cacheItemName].WaitAsync().ConfigureAwait(false);
                 try
                 {
                     cachedObject = await objectSettingFunction().ConfigureAwait(false);
@@ -87,23 +98,24 @@ namespace EasyMemoryCache
                     {
                         if (((ICollection)cachedObject).Count > 0 || cacheEmptyList)
                         {
-                            await _myCache.SetStringAsync(cacheItemName, serializedObject, entryOptions);
+                            await _myCache.SetStringAsync(cacheItemName, serializedObject, entryOptions).ConfigureAwait(false);
                         }
                     }
                     else
                     {
-                        await _myCache.SetStringAsync(cacheItemName, serializedObject, entryOptions);
+                        await _myCache.SetStringAsync(cacheItemName, serializedObject, entryOptions).ConfigureAwait(false);
                     }
                 }
                 catch (Exception err)
                 {
                     Console.WriteLine(err.Message);
-                    _cacheLock.Release();
                     return cachedObject;
                 }
+                finally
+                {
+                    _cacheLock[cacheItemName].Release();
+                }
             }
-
-            _cacheLock.Release();
             return cachedObject;
         }
 
@@ -130,7 +142,11 @@ namespace EasyMemoryCache
 
         public object GetValueFromCache(string key)
         {
-            return _myCache.Get(key);
+            var data = _myCache.GetString(key);
+            if (String.IsNullOrWhiteSpace(data))
+                return null;
+
+            return JsonConvert.DeserializeObject(data);
         }
 
         public void Dispose()
@@ -139,6 +155,7 @@ namespace EasyMemoryCache
             {
                 cache.Dispose();
             }
+            _cacheLock.Dispose();
         }
 
         public IEnumerable<string> GetKeys()
