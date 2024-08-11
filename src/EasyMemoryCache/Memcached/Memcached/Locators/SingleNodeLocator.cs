f2696830582e578f1d8e32cc4355d86bcc9f59e0
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2024-08-11 20:42:05 +0200

    Performance improvement for .NET 9.0+

--- src/EasyMemoryCache/Memcached/Memcached/Locators/SingleNodeLocator.cs
+++ src/EasyMemoryCache/Memcached/Memcached/Locators/SingleNodeLocator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace EasyMemoryCache.Memcached.Memcached.Locators
 {
@@ -11,7 +12,6 @@ namespace EasyMemoryCache.Memcached.Memcached.Locators
     {
         private IMemcachedNode node;
         private bool isInitialized;
-        private readonly object initLock = new Object();
 
         void IMemcachedNodeLocator.Initialize(IList<IMemcachedNode> nodes)
         {
